8650c1e6818a4a759ba6f29910a78357c826e59d
Diogo Mondego dos Santos <dm_dossantos@hotmail.com>
2022-06-11 15:01:27 -0300

    chore: defining schemas and models

---
+++ src/database/schemas/index.ts
@@ -0,0 +1,10 @@
+import { appSchema } from '@nozbe/watermelondb'
+
+import { skillSchema } from './skillSchema'
+
+export const schemas = appSchema({
+  version: 1,
+  tables: [
+    skillSchema,
+  ]
+})
