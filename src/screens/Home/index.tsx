b75d7348faff4f0fbab5f815a6a3a9715f6c4ec8
Diogo Mondego dos Santos <dm_dossantos@hotmail.com>
2022-06-11 16:45:04 -0300

    feat: home - create functions handleSave and fetchSkills

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,53 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { FlatList } from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
+import { Q } from '@nozbe/watermelondb'
 
 import { Menu, MenuTypeProps } from '../../components/Menu';
 import { Skill } from '../../components/Skill';
 import { Button } from '../../components/Button';
+
+import { database } from '../../database'
+import { SkillModel } from '../../database/models/skillModel';
 
 import { Container, Title, Input, Form, FormTitle } from './styles';
 
 export function Home() {
   const [type, setType] = useState<MenuTypeProps>("soft");
   const [name, setName] = useState('');
+  const [skills, setSkills] = useState<SkillModel[]>([]);
 
   const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const handleSave = async () => {
+    const skillsCollection = database.get<SkillModel>('skills');
+
+    await database.write(async () => {
+      await skillsCollection.create(data => {
+        data.name = name
+        data.type = type
+      })
+    })
+
+    fetchSkills()
+    setName('')
+    alert('Skill saved!')
+    bottomSheetRef.current.collapse()
+  }
+
+  const fetchSkills = async () => {
+    const skillsCollection = database.get<SkillModel>('skills');
+
+    const skillsResponse = await skillsCollection
+      .query( Q.where('type', type) )
+      .fetch()
+      
+    setSkills(skillsResponse)
+  }
+
+  useEffect(() => {
+    fetchSkills()
+  }, [type])
 
   return (
     <Container>
@@ -23,8 +58,8 @@ export function Home() {
       />
 
       <FlatList
-        data={[]}
-        keyExtractor={item => item}
+        data={skills}
+        keyExtractor={item => item.id}
         renderItem={({ item }) => (
           <Skill
             data={item}
@@ -50,7 +85,7 @@ export function Home() {
 
           <Button
             title="Save"
-            onPress={() => { }}
+            onPress={handleSave}
           />
         </Form>
       </BottomSheet>
